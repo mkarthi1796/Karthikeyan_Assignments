c02a2b4244eab28dc8ef1cd2a94530d8cb8c49f1
Karthikeyan M <122173451+mkarthi1796@users.noreply.github.com>
2023-01-08 10:13:51 +0530

    Update IsPrime.java

--- day1/IsPrime.java
+++ day1/IsPrime.java
@@ -4,33 +4,14 @@ public class IsPrime {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n=4;
-		if(n==1)
-		{
-			System.out.println("the number 1 is neither nor a prime number");
-
-		}
-		else if(n==2) {
-			System.out.println("the number 2 is prime number");
-		}
-
-		else {for (int i = 2; i <n; i++) {
-			if(n%i==0)
-			{
-				System.out.println("The Given Number "+n+" is not prime");
-				break;
-			}
-			else 
-			{
-				System.out.println("The Given Number "+n+" is prime");
+		int n = 9;
+		boolean prime = true;
+		for (int i = 2; i < n; i++) {
+			if (n % i == 0) {
+				prime = false;
 				break;
 			}
 		}
-		}
+		System.out.println("Status of the Given number " + n + " is Prime:" + prime);
 	}
 }
-
-
-
-
-
